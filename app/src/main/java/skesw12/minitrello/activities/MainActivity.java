2ea5ad529a7740f3d6e8096a6827e804b5419b6f
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-02-29 10:51:19 +0700

    Added method to add Card List to UI and Storage

--- app/src/main/java/skesw12/minitrello/activities/MainActivity.java
+++ app/src/main/java/skesw12/minitrello/activities/MainActivity.java
@@ -2,6 +2,11 @@ package skesw12.minitrello.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +14,15 @@ import java.util.List;
 import skesw12.minitrello.R;
 import skesw12.minitrello.models.CardList;
 import skesw12.minitrello.adapters.CardListAdapter;
+import skesw12.minitrello.models.Storage;
 import skesw12.minitrello.views.HorizontalListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText inputCardListTitle;
+    private TextView noCardLabel;
+    private Button addCardListButton;
+    private List<CardList> cardLists;
+    private CardListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,19 +30,46 @@ public class MainActivity extends AppCompatActivity {
         initComponent();
     }
     private void initComponent(){
-        List<CardList> cardLists = new ArrayList<CardList>();
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        cardLists.add(new CardList("SDAADSDAS"));
-        CardListAdapter adapter = new CardListAdapter(this,R.layout.cardlist_component,cardLists);
+        cardLists = new ArrayList<CardList>();
+        adapter = new CardListAdapter(this,R.layout.cardlist_component,cardLists);
         HorizontalListView horizontalListView = (HorizontalListView)findViewById(R.id.cardlist_hlistview);
         horizontalListView.setAdapter(adapter);
+        refresh();
+        addCardListButton = (Button)findViewById(R.id.add_cardlist_button);
+        noCardLabel = (TextView) findViewById(R.id.no_card_list_label);
+        inputCardListTitle = (EditText)findViewById(R.id.input_cardlist_box);
+        inputCardListTitle.setVisibility(View.GONE);
+        addCardListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCardListButton.setVisibility(View.GONE);
+                inputCardListTitle.setVisibility(View.VISIBLE);
+            }
+        });
+        inputCardListTitle.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    addNewCardList();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void addNewCardList() {
+        noCardLabel.setVisibility(View.GONE);
+        Storage.getInstance().addCardList(new CardList(inputCardListTitle.getText().toString()));
+        inputCardListTitle.setVisibility(View.GONE);
+        addCardListButton.setVisibility(View.VISIBLE);
+        refresh();
+    }
+
+    private void refresh(){
+        cardLists.clear();
+        for (CardList cl:Storage.getInstance().loadCardLists()) cardLists.add(cl);
+        adapter.notifyDataSetChanged();
     }
 
 
