c7c5028e4e38f7c5694e2c82831004c1b8e78be1
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-03-01 16:54:03 +0700

    A lot of bug here

---
+++ app/src/main/java/skesw12/minitrello/newactivities/MainActivity2.java
@@ -0,0 +1,15 @@
+package skesw12.minitrello.newactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import skesw12.minitrello.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
