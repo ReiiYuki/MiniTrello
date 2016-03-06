c2f52f045e05cfb910fc7fa9836f259dc118d0cf
nattapat sukpootanan <nattapat.su@ku.th>
2016-03-06 22:08:54 +0700

    change to Frangement

---
+++ app/src/main/java/skesw12/minitrello/adapters/PagerAdapter.java
@@ -0,0 +1,43 @@
+package skesw12.minitrello.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+import skesw12.minitrello.activities.CardFragments;
+import skesw12.minitrello.models.CardList;
+
+/**
+ * Created by nattapat on 3/6/2016 AD.
+ */
+public class PagerAdapter  extends FragmentStatePagerAdapter {
+    List<CardList> list;
+    private List<Fragment> fragments;
+    private FragmentManager fm;
+
+    public PagerAdapter(FragmentManager fm , List list) {
+        super(fm);
+        this.fm = fm;
+        this.list = list;
+        this.fragments = fragments;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return CardFragments.newInstance(list.get(position).getTitle(), "");
+    }
+
+//    public Fragment getActiveFragment(ViewPager container, int position) {
+//        String name = "android:switcher:" + container.getId() + ":" + position;
+//        return fm.findFragmentByTag(name);
+//    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
+
